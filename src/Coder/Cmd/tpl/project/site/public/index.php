5a81a9ec32baece9ad7a849fe5c0e4d682ec4800
zhanjh <zhanjh@126.com>
2017-12-06 20:06:07 +0800

    app-register

--- src/Coder/Cmd/tpl/project/site/public/index.php
+++ src/Coder/Cmd/tpl/project/site/public/index.php
@@ -19,86 +19,26 @@ if ($config->get('debug') !== false) {
 
 $app = new \Gap\Base\App($config);
 
-$app->set('router', function () use ($config) {
-    $srcOpts = [];
-    foreach ($config->get('app') as $appName => $appOpts) {
-        $srcOpts[$appName]['dir'] = $appOpts['dir']
-            . '/setting/router';
-    }
+register_router($app);
+register_session($app);
+register_view_engine($app);
+register_site_manager($app);
+register_site_url_builder($app);
+register_route_url_builder($app);
+// register_locale_manager($app);
+// register_dmg($app);
+// register_cmg($app);
+// register_translator($app);
 
-    $routerBuilder = new \Gap\Routing\RouterBuilder(
-        $config->get('baseDir'),
-        $srcOpts
-    );
-    if (false === $config->get('debug')) {
-        $routerBuilder
-            ->setCacheFile('cache/setting-router-http.php');
-    }
-    return $routerBuilder->build();
-});
-$app->set('session', function () use ($config) {
-    return obj(new \Gap\Site\SessionBuilder($config->get('session')))
-        ->build();
-});
-$app->set('viewEngine', function () use ($config) {
-    return \Foil\Foil::boot([
-        'folders' => [$config->get('baseDir') . '/view'],
-        'autoescape' => false,
-        'ext' => 'phtml'
-    ])->engine();
-});
+register_request_filter_manager($app, [
+    new \Gap\Base\RequestFilter\CsrfFilter()
+]);
+register_route_filter_manager($app, [
+    new \Gap\Base\RouteFilter\LoginFilter()
+]);
 
-$app->set('siteUrlBuilder', function () use ($app) {
-    return new \Gap\Routing\SiteUrlBuilder($app->get('siteManager'));
-});
-
-$app->set('routeUrlBuilder', function () use ($app) {
-    $routeUrlBuilder = new \Gap\Routing\RouteUrlBuilder(
-        $app->get('router'),
-        $app->get('siteUrlBuilder')
-    );
-
-    if ($app->has('localeManager')) {
-        $localeManager = $app->get('localeManager');
-        if ('path' === $localeManager->getMode()) {
-            $localeKey = $localeManager->getLocaleKey();
-            $localeKey = $localeKey ? $localeKey : $localeManager->getDefaultLocaleKey();
-            $routeUrlBuilder->setLocaleKey($localeKey);
-        }
-    }
-
-    return $routeUrlBuilder;
-});
-
-$app->set(
-    'siteManager',
-    \Gap\Routing\SiteManager::class,
-    [$config->get('site')]
-);
 /*
-$app->set(
-    'localeManager',
-    \Gap\I18n\Locale\LocaleManager::class,
-    [$config->get('i18n.locale')]
-);
-$app->set(
-    'dmg',
-    \Gap\Database\DatabaseManager::class,
-    [$config->get('db'), $config->get('server.id')]
-);
-
-$app->set(
-    'cmg',
-    \Gap\Cache\CacheManager::class,
-    [$config->get('cache'), $config->get('server.id')]
-);
-
-$app->set('translator', function () use ($app, $config) {
-    return new \Gap\I18n\Translator\Translator(
-        $app->get('dmg')->connect($config->get('i18n.db')),
-        $app->get('cmg')->connect($config->get('i18n.cache'))
-    );
-});
+// composer require gap/meta
 $app->set('meta', function () use ($app, $config) {
     return new \Gap\Meta\Meta(
         $app->get('dmg')->connect($config->get('meta.db')),
